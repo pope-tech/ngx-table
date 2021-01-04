22ad2a3561ca950ffa87d7fca5c9a71df83d9a75
Matt Moore <me@mattdoescode.com>
2021-01-04 12:31:35 -0700

    add tabindex to table

--- projects/ngx-table/src/lib/components/table/table.component.ts
+++ projects/ngx-table/src/lib/components/table/table.component.ts
@@ -66,7 +66,7 @@ export class DataTableComponent implements DataTableParams, OnInit, AfterContent
   // UI components:
   @ContentChildren(DataTableColumnDirective) columns: QueryList<DataTableColumnDirective>;
   @ViewChildren(DataTableRowComponent) rows: QueryList<DataTableRowComponent>;
-  
+
   @ViewChild(DataTablePaginationComponent, {static: false}) paginator;
 
   @ContentChild('dataTableExpand', { static: true }) expandTemplate: TemplateRef<any>;
