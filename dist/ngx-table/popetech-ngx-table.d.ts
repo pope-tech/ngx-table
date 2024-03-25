f6620b90c590fc3bb0fc1c5e6e735f267573bac1
Matt Moore <me@mattdoescode.com>
2024-03-25 16:50:38 -0600

    remove dist

--- dist/ngx-table/popetech-ngx-table.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { HideDirective as ɵb } from './lib/utils/hide';
-export { MinPipe as ɵc } from './lib/utils/min';
-export { PixelConverter as ɵa } from './lib/utils/px';
